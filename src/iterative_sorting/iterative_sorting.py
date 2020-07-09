c95d6652465f4b39ee9e423cef87fddfa26892d4
Luis Abellan <luis.abellan.dev@gmail.com>
2020-07-09 20:20:53 +0200

    selection and bubble_sort done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -9,18 +9,50 @@ def selection_sort(arr):
         # Your code here
 
 
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+
+
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
+
+# test code
+
+# arr = [3,12,4,7,2]
+# sorted = selection_sort(arr)
+# print(sorted)
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    n = len(arr)
+
+    # Traverse the array
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 
 
     return arr
+
+# Driver code to test above
+
+# arr = [3,12,4,7,2]
+# sorted = bubble_sort(arr)
+# print(sorted)
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -31,11 +63,11 @@ data. The idea behind this algorithm then is that we can create "buckets"
 from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
-Each buckets[i] then is responsible for keeping track of how many times 
+Each buckets[i] then is responsible for keeping track of how many times
 we've seen `i` in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
-showed up, we can construct a sorted set of the input data from the 
-buckets. 
+showed up, we can construct a sorted set of the input data from the
+buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
